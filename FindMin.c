128828a6cc0cfe089d388e77fcf1747685f07838
poh chiat <pohchiat@gmail.com>
2013-04-03 12:21:16 +0800

    Added implementation.

--- FindMin.c
+++ FindMin.c
@@ -8,6 +8,8 @@
  ============================================================================
  */
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include "mpi.h"
 
 int main(int argc, char* argv[]){
@@ -24,12 +26,36 @@ int main(int argc, char* argv[]){
 	/* find out number of processes */
 	MPI_Comm_size(MPI_COMM_WORLD, &p);
 
-	//randomly generate 3 int.
-	//find min sequentially
+	//randomly generate 10 int.
+    int arr[10];
+    srand(time(NULL) + my_rank);
+    int i;
+    for(i = 0; i < 10; ++i){
+    	arr[i] = rand() % 1000;
+    }
+
+ 	//find min sequentially
+ 	int min = arr[0];
+ 	for(i = 0; i < 10; ++i){
+ 		if(arr[i] < min){
+ 			min = arr[i];
+ 		}
+ 	}
+ 	printf("I am rank %d and found %d to be my minimum value.\n", my_rank, min);
 
 	//use reduce, mpi_minloc operation
+	struct{
+		int val;
+		int rank;
+	} in, out;
+	in.val = min;
+	in.rank = my_rank;
+	MPI_Reduce(&in, &out, 1, MPI_2INT, MPI_MINLOC, 0, MPI_COMM_WORLD ); 
 
 	//print out from root.
+	if(my_rank == 0){
+		printf("min value: %d from rank: %d\n", out.val, out.rank);
+	}
 
 	/* shut down MPI */
 	MPI_Finalize(); 
